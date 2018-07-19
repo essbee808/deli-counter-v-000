e6d2fba6d962f91a80df842d7762939985122d3e
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-19 00:03:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,9 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.length >
-    katz_deli = katz_deli.unshift
+  elsif katz_deli.length > 1
+    katz_deli.unshift
+    puts 
   end
 end
 
